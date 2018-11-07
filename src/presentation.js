cb450580bb0da6f8ef2402ca5225db410d270dd6
Igor Barsi <ibarsi92@gmail.com>
2018-11-07 18:40:33 -0500

    ✨ Add second intro slide, categorized under "Introduction" section

--- src/presentation.js
+++ src/presentation.js
@@ -23,6 +23,11 @@ const Presentation = () => (
         Igor Barsi
       </Text>
     </Slide>
+    <Slide transition={['slide']} bgColor="primary">
+      <Heading size={4} textAlign="left" textColor="secondary" caps>
+        Introduction
+      </Heading>
+    </Slide>
     <Slide transition={['slide']} bgColor="secondary">
       <Heading size={5} textAlign="left" textColor="primary">
         What is a situational question?
@@ -30,9 +35,7 @@ const Presentation = () => (
       <List>
         <Appear>
           <ListItem>
-            <Text>
-              Specific questions regarding events that may happen on the job
-            </Text>
+            <Text>Specific, regarding events that may happen on the job</Text>
           </ListItem>
         </Appear>
         <Appear>
@@ -43,6 +46,28 @@ const Presentation = () => (
         <Appear>
           <ListItem>
             <Text>Assess the candidate's behaviour</Text>
+          </ListItem>
+        </Appear>
+      </List>
+    </Slide>
+    <Slide transition={['slide']} bgColor="secondary">
+      <Heading size={5} textAlign="left" textColor="primary">
+        Why are situational questions challenging?
+      </Heading>
+      <List>
+        <Appear>
+          <ListItem>
+            <Text>Detailed recall of past experiences</Text>
+          </ListItem>
+        </Appear>
+        <Appear>
+          <ListItem>
+            <Text>Craft compelling stories on the spot</Text>
+          </ListItem>
+        </Appear>
+        <Appear>
+          <ListItem>
+            <Text>Don’t know what to expect</Text>
           </ListItem>
         </Appear>
       </List>
