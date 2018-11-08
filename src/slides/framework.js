4c0d1fc42598f0dec005f46782d2273aa51f2fa4
Igor Barsi <ibarsi92@gmail.com>
2018-11-07 19:21:50 -0500

    ♻️ Collapse list item styles into re-usable component

--- src/slides/framework.js
+++ src/slides/framework.js
@@ -10,6 +10,14 @@ import {
   SlideSet,
 } from 'spectacle';
 
+const ListItemText = ({ children, ...props }) => (
+  <ListItem style={{ display: 'block' }} padding="20px 40px 20px 0">
+    <Text textSize={25} {...props}>
+      {children}
+    </Text>
+  </ListItem>
+);
+
 export default (
   <SlideSet>
     <Slide transition={['slide']} bgColor="primary">
@@ -24,55 +32,39 @@ export default (
       <Layout>
         <Fill>
           <List>
-            <ListItem>
-              <Text textSize={25}>
-                Give an example of a goal you reached and how you achieved it.
-              </Text>
-            </ListItem>
-            <ListItem>
-              <Text textSize={25}>
-                Give an example of how you’ve gone above and beyond the call of
-                duty.
-              </Text>
-            </ListItem>
-            <ListItem>
-              <Text textSize={25}>
-                Tell me about how you worked effectively under pressure.
-              </Text>
-            </ListItem>
-            <ListItem>
-              <Text textSize={25}>
-                Describe a time when you made a suggestion to improve something
-                on the project that you were working on.
-              </Text>
-            </ListItem>
+            <ListItemText>
+              Give an example of a goal you reached and how you achieved it.
+            </ListItemText>
+            <ListItemText>
+              Give an example of how you’ve gone above and beyond the call of
+              duty.
+            </ListItemText>
+            <ListItemText>
+              Tell me about how you worked effectively under pressure.
+            </ListItemText>
+            <ListItemText>
+              Describe a time when you made a suggestion to improve something on
+              the project that you were working on.
+            </ListItemText>
           </List>
         </Fill>
         <Fill>
           <List>
-            <ListItem>
-              <Text textSize={25}>
-                Describe a decision you made that was unpopular and how you
-                handled implementing it.
-              </Text>
-            </ListItem>
-            <ListItem>
-              <Text textSize={25}>
-                Describe a time you were faced with a stressful situation. How
-                did you handle it?
-              </Text>
-            </ListItem>
-            <ListItem>
-              <Text textSize={25}>
-                Give an example of a time when you showed initiative to resolve
-                an issue or to de-risk a potential situation.
-              </Text>
-            </ListItem>
-            <ListItem>
-              <Text textSize={25}>
-                Have you handled a difficult situation with a co-worker? How?
-              </Text>
-            </ListItem>
+            <ListItemText>
+              Describe a decision you made that was unpopular and how you
+              handled implementing it.
+            </ListItemText>
+            <ListItemText>
+              Describe a time you were faced with a stressful situation. How did
+              you handle it?
+            </ListItemText>
+            <ListItemText>
+              Give an example of a time when you showed initiative to resolve an
+              issue or to de-risk a potential situation.
+            </ListItemText>
+            <ListItemText>
+              Have you handled a difficult situation with a co-worker? How?
+            </ListItemText>
           </List>
         </Fill>
       </Layout>
