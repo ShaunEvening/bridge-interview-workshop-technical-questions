881558230726d999d8005a1a0c828fbdaf4a708c
Igor Barsi <ibarsi92@gmail.com>
2018-11-11 13:00:00 -0500

    ♻️ Refactor intro lists to remove extrenuous `Text` components.

--- src/slides/intro.js
+++ src/slides/intro.js
@@ -1,13 +1,13 @@
 import React from 'react';
-import {
-  SlideSet,
-  Slide,
-  Heading,
-  List,
-  Appear,
-  ListItem,
-  Text,
-} from 'spectacle';
+import { SlideSet, Slide, Heading, List, Appear, ListItem } from 'spectacle';
+
+const AppearingListItem = ({ children, ...props }) => (
+  <Appear>
+    <ListItem textColor="tertiary" padding="20px 0" {...props}>
+      {children}
+    </ListItem>
+  </Appear>
+);
 
 export default (
   <SlideSet>
@@ -25,47 +25,29 @@ export default (
       <Heading size={5} textAlign="left" textColor="primary">
         What are behavioural questions?
       </Heading>
-      <List>
-        <Appear>
-          <ListItem>
-            <Text>Focus on how you handled work situations in the past</Text>
-          </ListItem>
-        </Appear>
-        <Appear>
-          <ListItem>
-            <Text>Non-technical</Text>
-          </ListItem>
-        </Appear>
-        <Appear>
-          <ListItem>
-            <Text>
-              Based on the premise that past behaviour is a strong predictor of
-              future success
-            </Text>
-          </ListItem>
-        </Appear>
+      <List style={{ 'list-style': 'none' }}>
+        <AppearingListItem>
+          Focus on how you handled work situations in the past
+        </AppearingListItem>
+        <AppearingListItem>Non-technical</AppearingListItem>
+        <AppearingListItem>
+          Based on the premise that past behaviour is a strong predictor of
+          future success
+        </AppearingListItem>
       </List>
     </Slide>
     <Slide transition={['slide']} bgColor="secondary">
       <Heading size={5} textAlign="left" textColor="primary">
         Why are behavioural questions challenging?
       </Heading>
-      <List>
-        <Appear>
-          <ListItem>
-            <Text>Detailed recall of past experiences</Text>
-          </ListItem>
-        </Appear>
-        <Appear>
-          <ListItem>
-            <Text>Craft compelling stories on the spot</Text>
-          </ListItem>
-        </Appear>
-        <Appear>
-          <ListItem>
-            <Text>Don’t know what to expect</Text>
-          </ListItem>
-        </Appear>
+      <List style={{ 'list-style': 'none' }}>
+        <AppearingListItem>
+          Require detailed recall of past experiences
+        </AppearingListItem>
+        <AppearingListItem>
+          Call for you to share telling stories on the spot
+        </AppearingListItem>
+        <AppearingListItem>You don’t know what to expect</AppearingListItem>
       </List>
     </Slide>
   </SlideSet>
